1403105d066badee0b92eee6daadcded0562444d
baraazaka <bara.zaka@outlook.com>
2026-08-09 21:26:43 +0300

    add controllers projects,getproject and getprojectById

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,6 @@
 const pool=requre("../db");
 
-const getProjects=async (req,res=>{
+const getProject=async (req,res=>{
     try{
         const result= await pool.qurey("select * from projects order by created_at desc");
         res.json(result.rows);
@@ -10,6 +10,21 @@ const getProjects=async (req,res=>{
     }
 });
 
+
+const getProjectById=async (req,res=>{
+    try{
+    const id = req.params.id;
+        const result=await pool.qurey("select * from projects where id=$1",[id]);
+        if(result.rows.length==0){
+            res.status(404).json({error:"project not found"});
+        }
+        res.json(result.rows[0]);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:"Failed to fetch projects"});
+    }
+});
 module.exports={
-    getProjects
+    getProjects,
+    getProjectById
 };
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -5,34 +5,10 @@ const router = express.Router();
 
 
 
-const{getProjects}=require("../controllers/projectController");
+const{getProjectById,getProjectById}=require("../controllers/projectController");
 router.get("/",getProjects);
 
-router.get("/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-
-        const result = await pool.query(
-            "SELECT * FROM projects WHERE id = $1",
-            [id]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                error: "Project not found"
-            });
-        }
-
-        res.json(result.rows[0]);
-
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            error: "Failed to fetch project"
-        });
-    }
-});
+router.get("/:id",getProjectsId);
 
 
 
