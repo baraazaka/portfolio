554c53295c7f4a9a85f4c9a46eef8f14a94a2090
baraazaka <bara.zaka@outlook.com>
2026-08-09 20:30:33 +0300

    Now, the APIs and database have been linked.

--- server.js
+++ server.js
@@ -1,60 +1,177 @@
 const express = require("express");
+const pool = require("./db");
 
 const app = express();
-
-const PORT = 5000;
 app.use(express.json());
-const projects = [
-    {
-        id: 1,
-        title: "Fabric AI",
-        description: "Fabric defect detection system"
-    },
-    {
-        id: 2,
-        title: "FunRoom",
-        description: "Multiplayer web application"
-    }
-];
+const PORT = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Portfolio API is running!");
+    res.send("Portfolio API is running!");
 });
-app.get("/api/projects",(req,res)=>{
-  res.json(projects);
-})
 
-app.post("/api/projects", (req, res) => {
-    const newProject = {
-        id: projects.length + 1,
-        title: req.body.title,
-        description: req.body.description
-    };
-
-    projects.push(newProject);
-
-    res.status(201).json(newProject);
+app.get("/test-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({
+            message: "Database connected!",
+            time: result.rows[0].now
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Database connection failed"
+        });
+    }
 });
-app.put("/api/projects/:id",(req,res)=>{
-  const projectId=parseInt(req.params.id);
-  const project=projects.find(project=>project.id==projectId)
-  if(!project){
-    return res.status(404).json({"message":"project not found"});
+app.get("/api/projects",async (req,res)=>{
+    try{
+      const result=await pool.query("select * from projects  order by created_at desc");
+      res.json(result.rows);
 
-  }
-  project.title=req.body.title;
-  project.description=req.body.description;
-  res.json(project);
+    }
+    catch(error){
+      console.error(error);
+      res.status(500).json({"error":"faild to fetch projects"});
+    }
 });
-app.delete("api/prjects/:id",(req,res)=>{
-  const prjectId=prearseInt(req.params.id);
-  const projectIndex=projects.findIndex(project=>project.id==prjectId);
-  if(projectIndex==-1){
-    return res.status(404).json({"message":"prject not found"});
-  }
-  const deletedProject=prjects.splice(prjectIndex,1);
-  res.json(deletedProject);
-})
+
+app.post("/api/projects", async (req, res) => {
+    try {
+        const {
+            user_id,
+            title,
+            description,
+            image_url,
+            github_url,
+            live_url
+        } = req.body;
+
+        const result = await pool.query(
+            `INSERT INTO projects
+            (user_id, title, description, image_url, github_url, live_url)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *`,
+            [
+                user_id,
+                title,
+                description,
+                image_url,
+                github_url,
+                live_url
+            ]
+        );
+
+        res.status(201).json(result.rows[0]);
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            error: "Failed to create project"
+        });
+    }
+});
+app.get("/api/projects/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            "SELECT * FROM projects WHERE id = $1",
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: "Project not found"
+            });
+        }
+
+        res.json(result.rows[0]);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            error: "Failed to fetch project"
+        });
+    }
+});
+app.put("/api/projects/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const {
+            title,
+            description,
+            image_url,
+            github_url,
+            live_url
+        } = req.body;
+
+        const result = await pool.query(
+            `UPDATE projects
+             SET title = $1,
+                 description = $2,
+                 image_url = $3,
+                 github_url = $4,
+                 live_url = $5,
+                 updated_at = NOW()
+             WHERE id = $6
+             RETURNING *`,
+            [
+                title,
+                description,
+                image_url,
+                github_url,
+                live_url,
+                id
+            ]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: "Project not found"
+            });
+        }
+
+        res.json(result.rows[0]);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            error: "Failed to update project"
+        });
+    }
+});
+app.delete("/api/projects/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const result = await pool.query(
+            "DELETE FROM projects WHERE id = $1 RETURNING *",
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: "Project not found"
+            });
+        }
+
+        res.json({
+            message: "Project deleted successfully",
+            project: result.rows[0]
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            error: "Failed to delete project"
+        });
+    }
+});
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
