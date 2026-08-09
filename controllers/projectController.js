00f40f62e9a4c29034ca3915fcd3901f6b62224f
baraazaka <bara.zaka@outlook.com>
2026-08-09 21:45:12 +0300

    add controller updateProject

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -24,7 +24,51 @@ const getProjectById=async (req,res=>{
         res.status(500).json({error:"Failed to fetch projects"});
     }
 });
+const createProject=async(req,res=>{
+    try{
+        const{user_id,titile,description,image_url,github_url,live_url}=req.body;
+        const result=await pool.qurey(
+            'insert into projects(user_id,title,description,image_url,github_url,live_url) values($1,$2,$3,$4,$5,$6)RETERNING*',
+            [
+                user_id,
+                titile,
+                description,
+                image_url,
+                github_url,
+                live_url
+
+            ]
+        );
+        res.status(201).json(result.rows[0]);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:"failed to created"});
+    }
+});
+const updateProject=async (req,res=>{
+    try{
+        const id=req.params.id;
+        const{title,description,image_url,github_url,live_url}
+        const result=await pool.qurey(
+            'update projects set title=$1,description=$2,image_url=$3,github_url=$4,live_url=$5 RETERNING*',
+            [title,description,image_url,github_url,live_url,id]);
+        
+            if(result.rows.length==0){
+                res.status(404).json({error:"projects not found"})
+            }
+            res.json(result.rows(0));
+
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:"failed to update"});
+    }
+});
+
+
 module.exports={
     getProjects,
-    getProjectById
+    getProjectById,
+    createProject,
+    updateProject
 };
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,103 +1,18 @@
 const express = require("express");
-const pool = require("../db");
 
 const router = express.Router();
 
 
 
-const{getProjectById,getProjectById}=require("../controllers/projectController");
-router.get("/",getProjects);
-
-router.get("/:id",getProjectsId);
-
-
-
-router.post("/", async (req, res) => {
-    try {
-        const {
-            user_id,
-            title,
-            description,
-            image_url,
-            github_url,
-            live_url
-        } = req.body;
-
-        const result = await pool.query(
-            `INSERT INTO projects
-            (user_id, title, description, image_url, github_url, live_url)
-            VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING *`,
-            [
-                user_id,
-                title,
-                description,
-                image_url,
-                github_url,
-                live_url
-            ]
-        );
-
-        res.status(201).json(result.rows[0]);
-
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            error: "Failed to create project"
-        });
-    }
-});
+const{getProject,getProjectById,createProject,updateProject}=require("../controllers/projectController");
+router.get("/",getProject);
+router.get("/:id",getProjectById);
+router.post("/",createProject);
+router.put("/:id",updateProject);
 
 
-router.put("/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
 
-        const {
-            title,
-            description,
-            image_url,
-            github_url,
-            live_url
-        } = req.body;
 
-        const result = await pool.query(
-            `UPDATE projects
-             SET title = $1,
-                 description = $2,
-                 image_url = $3,
-                 github_url = $4,
-                 live_url = $5,
-                 updated_at = NOW()
-             WHERE id = $6
-             RETURNING *`,
-            [
-                title,
-                description,
-                image_url,
-                github_url,
-                live_url,
-                id
-            ]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                error: "Project not found"
-            });
-        }
-
-        res.json(result.rows[0]);
-
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            error: "Failed to update project"
-        });
-    }
-});
 
 
 
