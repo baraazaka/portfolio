23ff36482ef42e3062833f62e60e36d4bdba63ba
baraazaka <bara.zaka@outlook.com>
2026-08-10 23:42:25 +0300

    add  2 api in messages(delete,and update)

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -48,8 +48,70 @@ const getMessageById = async (req, res) => {
     }
 };
 
+const createMessage = async (req, res) => {
+    try {
+        const {
+            name,
+            email,
+            message
+        } = req.body;
 
+        const result = await pool.query(
+            `INSERT INTO messages
+            (name, email, message)
+            VALUES ($1, $2, $3)
+            RETURNING id, name, email, message, created_at`,
+            [
+                name,
+                email,
+                message
+            ]
+        );
+
+        res.status(201).json(result.rows[0]);
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            error: "Failed to create message"
+        });
+    }
+};
+
+const deleteMessage = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const result = await pool.query(
+            `DELETE FROM messages
+             WHERE id = $1
+             RETURNING id, name, email, message`,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: "Message not found"
+            });
+        }
+
+        res.json({
+            message: "Message deleted successfully",
+            deletedMessage: result.rows[0]
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            error: "Failed to delete message"
+        });
+    }
+};
 module.exports = {
     getMessages,
-    getMessageById
+    getMessageById,
+    createMessage,
+    deleteMessage
 };
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const {
     getMessages,
-    getMessageById
+    getMessageById,
+    createMessage,
+    deleteMessage
 } = require("../controllers/messageController");
 
 const router = express.Router();
 
 router.get("/", getMessages);
 router.get("/:id", getMessageById);
+router.post("/",createMessage);
+router.delete("/:id",deleteMessage);
 
 module.exports = router;
