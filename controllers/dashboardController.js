84740f8ace760b8c6c75431439ecbfb385228b54
baraazaka <bara.zaka@outlook.com>
2026-08-15 21:18:53 +0300

    Add dashboard api get

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -16,12 +16,29 @@ const getDashboardStats = async (req, res) => {
              "SELECT COUNT(*) FROM experiences WHERE user_id = $1",
                 [userId]);
 
-                 res.json({
-        projectsCount: Number(projects.rows[0].count),
-        skillsCount: Number(skills.rows[0].count),
-        experiencesCount: Number(experiences.rows[0].count)
-    });
+            const recentProjects = await pool.query(
+                `SELECT id, title, description, image_url, github_url, live_url, created_at
+                 FROM projects
+                 WHERE user_id = $1
+                 ORDER BY created_at DESC
+                 LIMIT 5`,
+                [userId]);  
 
+            const recentExperiences = await pool.query(
+                        `SELECT id, company, postion, description, start_date, end_date
+                        FROM experiences
+                        WHERE user_id = $1
+                        ORDER BY start_date DESC
+                        LIMIT 5`,
+                        [userId]); 
+                res.json({
+                projectsCount: Number(projects.rows[0].count),
+                skillsCount: Number(skills.rows[0].count),
+                experiencesCount: Number(experiences.rows[0].count),
+                recentProjects: recentProjects.rows,
+                recentExperiences: recentExperiences.rows
+            });
+              
     } catch (error) {
         console.log(error);
 
