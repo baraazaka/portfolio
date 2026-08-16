781ae4df1551ed781f1da074846ccbd35edc11d6
baraazaka <bara.zaka@outlook.com>
2026-08-16 23:08:42 +0300

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>My Portfolio</h1>
+
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,24 @@
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+
 function Home() {
     return(
-        <h1>Home Page</h1>
+        <>
+        <Navbar/>
+        <section>
+
+            <h1>Hi,I'am Baraa</h1>
+            <h2>Full-Stack Developer</h2>
+            <p>
+                    I build modern web applications using React and Node.js.
+            </p>
+
+            <Link to="/projects">
+                    View Projects
+                </Link>
+        </section>
+        
+        </>
     );
 }
 export default Home;
