e8a309e316ed36450a1d4c2bd70480242f0bb072
baraazaka <bara.zaka@outlook.com>
2026-08-17 15:34:19 +0300

    Add protected dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./pages/Dashboard";
 
 function App(){
   return(
@@ -11,6 +13,7 @@ function App(){
       <Route path="/" element={<Home/>}/>
       <Route path="/Login" element={<Login/>}/>
       <Route path="/Register" element={<Register/>}/>
+      <Route path="/Dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
 
     </Routes>
     </BrowserRouter>
